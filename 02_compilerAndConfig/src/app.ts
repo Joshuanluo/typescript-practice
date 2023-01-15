d533cb206ac4b8acd55b1b89e6fdb652f5dfff5a
Joshuanluo <joshua.chuan.luo@gmail.com>
2023-01-15 23:03:07 +1100

    compiler and tsconfig

---
+++ 02_compilerAndConfig/src/app.ts
@@ -0,0 +1,12 @@
+const userName ='josh';
+
+console.log(userName);
+
+const button = document.querySelector('button')!;
+
+button.addEventListener('click',()=>{
+    console.log('clicked!!!');
+    
+})
+
+//this is a comment
