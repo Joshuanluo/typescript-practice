4863d3a941327f6d114e3505db7cd03c861d02b2
Joshuanluo <joshua.chuan.luo@gmail.com>
2023-01-18 18:56:41 +1100

    modern js feature

--- 03_module/src/app.ts
+++ 03_module/src/app.ts
@@ -1,7 +1,7 @@
 const userName = "josh";
 // userName = "joshua"
-let age = 30;
-age = 20;
+let age1 = 30;
+age1 = 20;
 
 function add(a: number, b: number) {
 	var result;
@@ -11,7 +11,7 @@ function add(a: number, b: number) {
 
 // console.log(result); //error
 
-const addition = (a: number=111, b: number = 111) => a + b;
+const addition = (a: number, b: number = 111) => a + b;
 
 const printOut :(a:number|string) => void = output => console.log(output);
 printOut(addition(5));
@@ -21,3 +21,29 @@ const button = document.querySelector('button');
 if(button){
     button.addEventListener('click', event => console.log(event))
 }
+
+const hobbies = ['Sports','Cooking'];
+const activeHobbies = ['Hiking',...hobbies];
+activeHobbies.push(...hobbies);
+
+const person={
+    firstName:'josh',
+    age:30
+}
+const  copiedPerson = {...person};
+
+const add1=(...numbers:number[])=>{
+    
+    return numbers.reduce((curResult, curValue)=>{
+        return curResult+curValue;
+    })
+}
+const addNumbers= add1(5,10,2,3.7);
+console.log(addNumbers);
+
+const [hobby1,hobby2,...remainingHobbies] = hobbies;
+
+console.log(hobbies,hobby1,hobby2,remainingHobbies);
+
+const {firstName:lastName, age} =person;
+console.log(lastName,age,person);
