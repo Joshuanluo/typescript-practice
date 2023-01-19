8d38b07a21b8a513039e1aea0dc760014796263f
Joshuanluo <joshua.chuan.luo@gmail.com>
2023-01-19 21:01:00 +1100

    static methods and properties

--- 04_classAndInterface/src/app.ts
+++ 04_classAndInterface/src/app.ts
@@ -1,83 +1,113 @@
-class Department{
+class Department {
+    static fiscalYear = 2023;
     // private readonly id: string;
-    // private name:string;
-    protected employees: string[]=[];
-
-    constructor(private readonly id:string,public name:string){
-        // this.id = id;
-        // this.name=n;
-    }
-    describe(this: Department){
-        console.log(`Department (${this.id}): ${this.name}`);
-        
-    }
+	// private name:string;
+	protected employees: string[] = [];
     
-    addEmployee(employee:string){
-        // this.id='';//readonly
-        this.employees.push(employee)
-    }
-
-    printEmployeeInformation(){
-        console.log(this.employees.length);
-        console.log(this.employees);
+	constructor(private readonly id: string, public name: string) {
+        // this.id = id;
+		// this.name=n;
+        //console.log(Department.fiscalYear);
         
+	}
+
+    static createEmployee(name: string){
+        return{name:name}
     }
+
+	describe(this: Department) {
+		console.log(`Department (${this.id}): ${this.name}`);
+	}
+
+	addEmployee(employee: string) {
+		// this.id='';//readonly
+		this.employees.push(employee);
+	}
+
+	printEmployeeInformation() {
+		console.log(this.employees.length);
+		console.log(this.employees);
+	}
 }
 
-class ITDepartment extends Department{
-    constructor(id:string,public admins:string[]){
-        super(id,'IT')
-        this.admins=admins;
-    }
+class ITDepartment extends Department {
+    constructor(id: string, public admins: string[]) {
+        super(id, "IT");
+		this.admins = admins;
+	}
 }
 
-class AccountingDepartment extends Department{
-    constructor(id:string,private reports:string[]){
-        super(id,'IT');
-    }
+class AccountingDepartment extends Department {
+    private lastReport: string;
+    
+	get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+		}
+		throw new Error("No report found.");
+	}
 
-    addEmployee(name: string){
-        if(name==='Max'){
-            return;
-        }
-        this.employees.push(name);
-    }
+	set mostRecentReport(value: string) {
+		if (!value) {
+			throw new Error("Please pass in a val");
+		}
+		this.addReport(value);
+	}
 
-    addReport(text:string){
-        this.reports.push(text);
-    }
+	constructor(id: string, private reports: string[]) {
+		super(id, "IT");
+		this.lastReport = reports[0];
+	}
 
-    printReports(){
-        console.log(this.reports);
-        
-    }
+	addEmployee(name: string) {
+		if (name === "Max") {
+			return;
+		}
+		this.employees.push(name);
+	}
+
+	addReport(text: string) {
+		this.reports.push(text);
+		this.lastReport = text;
+	}
+
+	printReports() {
+		console.log(this.reports);
+	}
 }
 
-const it=new ITDepartment('d1',["Max"]);
+const employee1 = Department.createEmployee('Max');
+console.log(employee1, Department.fiscalYear);
+
+
+const it = new ITDepartment("d1", ["Max"]);
+
+
 
 it.addEmployee("Max");
 it.addEmployee("Man");
 // it.employees[2] = 'Anna';//private property
 
 it.describe();
-it.name="new name";
-it.printEmployeeInformation();  
+it.name = "new name";
+it.printEmployeeInformation();
 
 console.log(it);
 
-const accounting = new AccountingDepartment('d2',['first report'])
-accounting.addReport('second report');
-accounting.addEmployee('jane');
+const accounting = new AccountingDepartment("d2", []);
+accounting.mostRecentReport="val";
+console.log(accounting.mostRecentReport);
 
-accounting.addEmployee('Max');
-accounting.addEmployee('Josh');
+accounting.addReport("second report");
+accounting.addEmployee("jane");
 
+accounting.addEmployee("Max");
+accounting.addEmployee("Josh");
 
 accounting.printReports();
 console.log(accounting);
-accounting.printEmployeeInformation()
+accounting.printEmployeeInformation();
 
 // const accountingCopy={name:'s',describe: accounting.describe}
 
 // accountingCopy.describe();
-
