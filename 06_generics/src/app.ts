4c9526060fa22b84d33f9ce136681f41868dcb74
Joshuanluo <joshua.chuan.luo@gmail.com>
2023-01-26 12:09:05 +1100

    another generic function

--- 06_generics/src/app.ts
+++ 06_generics/src/app.ts
@@ -19,3 +19,20 @@ function merge<T extends object, U extends object>(objA: U, objB: T) {
 const mergeObj = merge({ name: "josh", hobbies: ["Sports"] }, { age: 18 });
 
 console.log(mergeObj);
+
+interface Lengthy{
+    length:number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string]{
+    let describptionText = 'Got no value.';
+    if (element.length===1) {
+        describptionText='Got 1 element.'
+    } else if(element.length>1){
+        describptionText = 'Got '+element.length+' elements.'
+    }
+    return [element,describptionText];
+}
+
+console.log(countAndDescribe("Hi, there"));
+console.log(countAndDescribe(["Hi","there"]));
