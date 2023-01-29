e4262fcdd77974625a1aebb3dd9493cad669ee8e
Joshuanluo <joshua.chuan.luo@gmail.com>
2023-01-29 11:26:56 +1100

    create an Autobind decorator

--- 07_decorators/src/app.ts
+++ 07_decorators/src/app.ts
@@ -8,15 +8,19 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
-	return function (constructor: any) {
-		console.log("TEMPLATE FACTORY");
-
-		const hookEl = document.getElementById(hookId);
-		const p = new constructor();
-		if (hookEl) {
-			hookEl.innerHTML = template;
-			hookEl.querySelector("h2")!.textContent = p.name;
-		}
+	console.log("TEMPLATE FACTORY");
+	return function<T extends {new(...args:any[]): {name: string}}> (originalConstructor: T) {
+		return class extends originalConstructor {
+			constructor(..._: any[]) {
+				super();
+                console.log('Rendering template');
+				const hookEl = document.getElementById(hookId);
+				if (hookEl) {
+					hookEl.innerHTML = template;
+					hookEl.querySelector("h2")!.textContent = this.name;
+				}
+			}
+		};
 	};
 }
 
@@ -29,47 +33,45 @@ class Person {
 	}
 }
 
-const pers = new Person();
+// const pers = new Person();
 
-console.log(pers);
+// console.log(pers);
 
 /////////////////////
 
 function Log(target: any, propertyName: string | Symbol) {
-    console.log('Property decorator');
-    console.log(target, propertyName);
-    
+	console.log("Property decorator");
+	console.log(target, propertyName);
 }
 
-function Log2(target: any, name: string, descriptor:PropertyDescriptor){
-    console.log('Accessor decorator!');
-    
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+	console.log("Accessor decorator!");
+
+	console.log(target);
+	console.log(name);
+	console.log(descriptor);
 }
 
-function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor){
-    console.log('Method decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+	console.log("Method decorator!");
+	console.log(target);
+	console.log(name);
+	console.log(descriptor);
 }
 
-function Log4(target: any, name: string | Symbol, position: number){
-    console.log('Parameter decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(position);
+function Log4(target: any, name: string | Symbol, position: number) {
+	console.log("Parameter decorator!");
+	console.log(target);
+	console.log(name);
+	console.log(position);
 }
-
 
 class Product {
-    @Log
+	@Log
 	title: string;
 	private _price: number;
 
-    @Log2
+	@Log2
 	set price(val: number) {
 		if (val > 0) {
 			this._price = val;
@@ -82,8 +84,36 @@ class Product {
 		this._price = p;
 	}
 
-    @Log3
+	@Log3
 	getPriceWithTax(@Log4 tax: number) {
 		return this._price * (1 + tax);
 	}
 }
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor){
+    const originalMethod = descriptor.value;
+    const adjDescriptor : PropertyDescriptor ={
+        configurable: true,
+        enumerable: false,
+        get(){
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    }
+    return adjDescriptor;
+}
+
+class Printer {
+    message = 'This works!';
+
+    @Autobind
+    showMessage(){
+        console.log(this.message);
+        
+    }
+}
+
+const p = new Printer();
+
+const button = document.querySelector('button')!;
+button.addEventListener('click',p.showMessage);
